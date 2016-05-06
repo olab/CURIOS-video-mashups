a5254f22f7d9b1d5b7e57088c1a009e21c8ee18b
sergey-rud <sergey.rud@itrexgroup.com>
2016-05-06 10:41:07 +0300

    moved app config to .env file

--- example.env.php
+++ example.env.php
@@ -2,7 +2,10 @@
 
 return [
     'DB_HOST' => 'localhost',
-    'DB_DATABASE' => 'youtube_service',
+    'DB_DATABASE' => 'curios_video_player',
     'DB_USERNAME' => 'root',
     'DB_PASSWORD' => 'root',
+
+    'APP_DEBUG' => false,
+    'BASE_URL' => 'http://videoplayer/',
 ];
